8df9e543f8dd06e1de5fc07b74110da2de6e7f40
jatin thakur <tjatin29@gmail.com>
2023-01-25 10:28:02 +0530

    log in

--- lib/Views/loginviews.dart
+++ lib/Views/loginviews.dart
@@ -1,35 +1,10 @@
+
+
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:mynotes/firebase_options.dart';
-
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(
-    MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const LoginView(),
-    ),
-  );
-}
-
-
-class RegisterView extends StatefulWidget {
-  const RegisterView({super.key});
-
-  @override
-  State<RegisterView> createState() => _RegisterViewState();
-}
-
-class _RegisterViewState extends State<RegisterView> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
