ed156f8472dcd33694138f030056fc638f5c3bb7
jatin thakur <tjatin29@gmail.com>
2023-03-04 12:09:47 +0530

    doing chapter 24

---
+++ lib/services/auth/auth_services.dart
@@ -0,0 +1,19 @@
+//login exceptions 
+class UserNotFoundAuthExacption implements Exception{}
+
+class WrongPasspordAuthException implements Exception{}
+
+//Register Exceptions 
+ class WeakPasswordAuthException implements Exception{}
+
+ class EmailAlreadyInUseAuthException implements Exception{}
+
+ class InvalidEmailAuthException implements Exception{}
+
+ //Generic Exception
+
+ class GenericAuthException implements Exception{}
+
+ class UserNotLoggedInAuthException implements Exception{}
+
+
